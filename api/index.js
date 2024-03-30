aebf513f7d5c5c5ea053aa0f6b8af409d76b5964
vivek <vivekkohli935@gmail.com>
2024-03-30 14:31:27 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
-
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config()
 
@@ -13,10 +13,11 @@ mongoose.connect(process.env.MONGO).then(()=>{
 })
 
 const app = express()
- 
+app.use(express.json()) // this will allow to automatically parse all the json in req.body
 const port  = 3000
 app.listen(port, ()=>{  
     console.log(`Server listening on ${port}`)
 })
 
 app.use('/api/user', userRoutes)
+app.use('/api/auth', authRoutes)
