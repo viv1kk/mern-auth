744ea458e34bf520550a8d688d572df5b803ab10
vivek <vivekkohli935@gmail.com>
2024-04-02 01:54:05 +0530

    create update user api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
+import cookieParser from 'cookie-parser'
 
 dotenv.config()
 
@@ -14,6 +15,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 const app = express()
 app.use(express.json()) // this will allow to automatically parse all the json in req.body
+app.use(cookieParser())
 const port  = 3000
 app.listen(port, ()=>{  
     console.log(`Server listening on ${port}`)
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,10 @@
 import express from 'express'
 import { test } from '../controllers/user.controller.js'
+import { verifyToken } from '../utils/verifyUsers.js'
+import { updateUser } from '../controllers/user.controller.js'
 
 const router = express.Router()
 
 router.get('/', test)
+router.post('/update/:id', verifyToken, updateUser)
 export default router
