0c23ece930d8b4e12959c1bc3336b8237260879f
vivek <vivekkohli935@gmail.com>
2024-04-02 03:12:50 +0530

    add pre production changes

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
 import cookieParser from 'cookie-parser'
+import path from 'path'
 
 dotenv.config()
 
@@ -13,7 +14,15 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log(err)
 })
 
+
+const __dirname = path.resolve()
 const app = express()
+
+app.use(express.static(path.join(__dirname, '/client/dist')))
+app.get('*', (req, res)=>{
+    req.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
+
 app.use(express.json()) // this will allow to automatically parse all the json in req.body
 app.use(cookieParser())
 const port  = 3000
