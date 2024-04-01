b41ee034d9fbd6e5f2a57ac9aecebdc12648c52a
vivek <vivekkohli935@gmail.com>
2024-04-02 03:05:32 +0530

    complete home and about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,12 @@
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className="text-3xl font-bold mb-4 text-slate-700">About</h1>
+      <p className="mb-4 text-slate-700">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius dolorem maiores quaerat vel laborum soluta consectetur assumenda molestiae nihil nesciunt animi odit numquam architecto et officiis blanditiis vero nisi perferendis possimus enim praesentium, nemo minima cum. Laudantium dicta vel voluptates? Amet perferendis, ducimus dolore id in et numquam beatae sed.</p>
+      <p className="mb-4 text-slate-700">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius dolorem maiores quaerat vel laborum soluta consectetur assumenda molestiae nihil nesciunt animi odit numquam architecto et officiis blanditiis vero nisi perferendis possimus enim praesentium, nemo minima cum. Laudantium dicta vel voluptates? Amet perferendis, ducimus dolore id in et numquam beatae sed.</p>
+      <p className="mb-4 text-slate-700">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius dolorem maiores quaerat vel laborum soluta consectetur assumenda molestiae nihil nesciunt animi odit numquam architecto et officiis blanditiis vero nisi perferendis possimus enim praesentium, nemo minima cum. Laudantium dicta vel voluptates? Amet perferendis, ducimus dolore id in et numquam beatae sed.</p>
+    </div>
   )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,12 @@
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className="text-3xl font-bold mb-4 text-slate-700">Welcome to my Auth App!</h1>
+      <p className="mb-4 text-slate-700">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius dolorem maiores quaerat vel laborum soluta consectetur assumenda molestiae nihil nesciunt animi odit numquam architecto et officiis blanditiis vero nisi perferendis possimus enim praesentium, nemo minima cum. Laudantium dicta vel voluptates? Amet perferendis, ducimus dolore id in et numquam beatae sed.</p>
+      <p className="mb-4 text-slate-700">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius dolorem maiores quaerat vel laborum soluta consectetur assumenda molestiae nihil nesciunt animi odit numquam architecto et officiis blanditiis vero nisi perferendis possimus enim praesentium, nemo minima cum. Laudantium dicta vel voluptates? Amet perferendis, ducimus dolore id in et numquam beatae sed.</p>
+      <p className="mb-4 text-slate-700">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius dolorem maiores quaerat vel laborum soluta consectetur assumenda molestiae nihil nesciunt animi odit numquam architecto et officiis blanditiis vero nisi perferendis possimus enim praesentium, nemo minima cum. Laudantium dicta vel voluptates? Amet perferendis, ducimus dolore id in et numquam beatae sed.</p>
+    </div>
   )
 }
 
